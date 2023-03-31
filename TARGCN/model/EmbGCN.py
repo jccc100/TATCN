1f0721ed5ca2520f15afd7b9028a0e58b0dfdc7a
jccc100 <2428791113@qq.com>
2023-03-31 10:35:54 +0800

    2 gcn

--- TARGCN/model/EmbGCN.py
+++ TARGCN/model/EmbGCN.py
@@ -88,19 +88,24 @@ class EmbGCN(nn.Module):
         supports = torch.eye(node_num).to(supports.device)+supports
         #static
         x_static = torch.einsum("nm,btmc->btnc",torch.softmax(self.sym_norm_Adj_matrix,dim=-1),x)
+        x_static = torch.einsum("nm,btmc->btnc",torch.softmax(self.sym_norm_Adj_matrix,dim=-1),x_static)
         # x_static = self.linear(x_static) # btnc
         x_static = torch.relu(x_static)
 
         #spatial attention
         x_sa = torch.relu(self.SA(x, self.sym_norm_Adj_matrix))
+        x_sa = torch.relu(self.SA(x_sa, self.sym_norm_Adj_matrix))
 
         weights = torch.einsum('nd,dio->nio', node_embeddings1, self.weights_pool)  #N, cheb_k, dim_in, dim_out
         bias = torch.matmul(node_embeddings1, self.bias_pool)#N, dim_out
 
         x_g = torch.einsum("nm,btmc->btnc", supports, x)
         x_g = torch.einsum("nm,btmc->btnc", supports, x_g)
+        x_g = torch.einsum("nm,btmc->btnc", supports, x_g)
+
 
         x_gconv = torch.einsum('btni,nio->btno', x_g, weights) + bias     #b, N, dim_out
+
         x_gconv = torch.relu(x_gconv)
         # return x_gconv+torch.sigmoid(x_static)*x_static+x_sa
         return self.alpha*x_gconv+self.beta*x_sa+self.gamma*x_static
